1bd4444c5af00529aab402476a7c981c47a9b7f6
migueldlozano <miguel.d.lozano@gmail.com>
2017-01-10 20:33:21 -0500

    Add @classmethod and @staticmethod

--- oop/classes.py
+++ oop/classes.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Employee():
 
     # class variables
@@ -21,26 +23,48 @@ class Employee():
         self.pay = int(self.pay * self.raise_amount)
 
 
+    @classmethod
+    def set_raise_amt(cls, amt):
+        cls.raise_amount = amt
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
 print(Employee.employee_count)
 emp_1 = Employee('Miguel', 'Lozano', 55000)
 emp_2 = Employee('Test', 'User', 60000)
 
-# print(emp_1.__dict__)
-print(emp_1.full_name())
-print(emp_1.email)
+my_date = datetime.date(2017, 1, 11)
 
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
+print(Employee.is_workday(my_date))
 
-
-print(Employee.employee_count)
-# print(Employee.raise_amount)
+# # print(emp_1.__dict__)
+# print(emp_1.full_name())
+# print(emp_1.email)
+#
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
+#
+#
+# print(Employee.employee_count)
+# # print(Employee.raise_amount)
+# # print(emp_1.raise_amount)
+# # emp_1.raise_amount = 2
 # print(emp_1.raise_amount)
-# emp_1.raise_amount = 2
+# Employee.set_raise_amt(2.0)
 # print(emp_1.raise_amount)
-# print(emp_2.raise_amount)
-
-
-print(emp_2.full_name())
-print(emp_2.email)
+# # print(emp_2.raise_amount)
+#
+#
+# print(emp_2.full_name())
+# print(emp_2.email)
